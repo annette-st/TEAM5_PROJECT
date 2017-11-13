3b10826fef6517c933031feb799869feab5cab4f
MaximPozdeev <maksim.g14@mail.ru>
2017-11-13 21:51:50 +0300

    13.11.17. added: GUI start page

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    public void start(ActionEvent actionEvent) {
+
+    }
+
+    public void showCredits(ActionEvent actionEvent) {
+    }
+
+    public void showRules(ActionEvent actionEvent) {
+    }
+
+    public void quit(ActionEvent actionEvent) {
+    }
 }
