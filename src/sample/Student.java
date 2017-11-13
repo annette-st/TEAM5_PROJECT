36e01e01888b1e37cec1d47487adcf4c15eeb43d
razil <razil0071999@gmail.com>
2017-11-13 23:50:48 +0300

    date 12.11.2017 реализация самой игры в методе toHack()

--- src/sample/Student.java
+++ src/sample/Student.java
@@ -1,37 +1,122 @@
 package sample;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Student {
+    Scanner scanner = new Scanner(System.in);
     int satiety;
     int energy;
-    int univer;
+    int university;
     int money;
 
-    public Student(int satiety, int energy, int univer, int money) {
+    public Student(int satiety, int energy, int university, int money) {
         this.satiety = satiety;
         this.energy = energy;
-        this.univer = univer;
+        this.university = university;
         this.money = money;
     }
 
     public void toHack() {
+
+        final int attempts = 3; //кол-во попыток
+        boolean end = false; // конец игры (true - игра кончилась с положительным исходом, false - с отрицательным)
+        //что будет выводиться
+        char output[] = new char[4];
+        for (int i = 0; i <output.length; i++) {
+            output[i] = '*';
+            System.out.print(output[i]); ///////////////////////////////////////////
+        }
+        System.out.println(" "); //////////////////////////////////////////////////
+        //цифры - коровы
+
+
+
         int cardID[] = new int[4];
+        //генерация рандомного числа
         Random rnd = new Random(System.currentTimeMillis());
         boolean digits[] = new boolean[10];
         for (int i = 0; i < digits.length; i++)
             digits[i] = true;
-        for(int i = 0; i < cardID.length; i++) {
+        for (int i = 0; i < cardID.length; i++) {
             do {
                 cardID[i] = rnd.nextInt(10);
             }
             while (digits[cardID[i]] != true);
             digits[cardID[i]] = false;
         }
+        //игра
 
-       // int inputID[] = new int[4];
+        for (int n = 0; n < attempts; n++) {
 
+            for (int x : cardID) {
+                System.out.print(x);
+            }
+            System.out.println();
+            //ввод 4-значного числа
+            System.out.println("Введите щисло");
+            int input = scanner.nextInt(); ////////////////////////////////////////////// вводимое число
+            int inputID[] = new int[4];
 
+            // формирование числа в массив цифр
+            for (int i = 0; i < inputID.length; i++) {
+                inputID[inputID.length - i -1] = input % 10;
+                input /= 10;
+            }
+            //нахождение коров
+            boolean cowdigit[] = new boolean[10];
+            for (int i = 0; i < cowdigit.length; i++)
+                cowdigit[i] = false;
 
+            for (int i = 0; i < cardID.length; i++)
+                for (int j = 0; j < inputID.length; j++) {
+                    if (cardID[i] == inputID[j] && i != j ) {
+                        cowdigit[cardID[i]] = true;
+                    }
+                }
+            //нахождение быков
+            if (inputID[0] == cardID[0]) {
+                output[0] = Character.forDigit(inputID[0], 10);
+                cowdigit[inputID[0]] = false;
+            }
+            if (inputID[1] == cardID[1]) {
+                output[1] = Character.forDigit(inputID[1], 10);
+                cowdigit[inputID[1]] = false;
+            }
+            if (inputID[2] == cardID[2]) {
+                output[2] = Character.forDigit(inputID[2], 10);
+                cowdigit[inputID[2]] = false;
+            }
+            if (inputID[3] == cardID[3]) {
+                output[3] = Character.forDigit(inputID[3], 10);
+                cowdigit[inputID[3]] = false;
+            }
+
+            //высчитывание конца игры(проверка на полное совпадение 2 кодов - рандомного и вводимого)
+            end = true;
+            for (int i = 0; i < cardID.length; i++) {
+                if (output[i] != Character.forDigit(cardID[i], 10))
+                    end &= false;
+            }
+            if (end == true) break;
+
+            //принты
+            for (int i = 0; i < output.length; i++) {
+                System.out.print(output[i]);
+            }
+            System.out.println(" ");
+            System.out.print("Коровы: ");
+            for (int i = 0; i < cowdigit.length; i++){
+                if (cowdigit[i] == true)
+                    System.out.print(i + " ");
+            }
+            System.out.println(" ");
+        }
+
+        if (end == true) // действие при конце игры с положительным исходом
+            System.out.println("игру выиграл");
+        else { //с отрицательным
+            System.out.println("игру не выиграл");
+        }
     }
 }
