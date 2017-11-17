8630203950372c5f79b59d38a378b13f9a8ab40b
MaximPozdeev <maksim.g14@mail.ru>
2017-11-18 02:18:29 +0300

    15.11.17. done: new packages

--- src/sample/classes/Main.java
+++ src/sample/classes/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package sample.classes;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxmlFiles/mainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../fxmlFiles/mainWindow.fxml"));
         primaryStage.setTitle("The Game");
         primaryStage.setScene(new Scene(root, 450, 475));
         primaryStage.setResizable(false);
--- src/sample/classes/Student.java
+++ src/sample/classes/Student.java
@@ -1,4 +1,4 @@
-package sample;
+package sample.classes;
 
 import java.util.Random;
 import java.util.Scanner;
